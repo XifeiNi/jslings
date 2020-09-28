3ffba947f0b1433280cd20353039aaf36c7a9f82
Anthony Morris <anthony@amorrissound.com>
2020-09-28 08:05:22 -0700

    Add first number test

---
+++ exercises/numbers/numbers1.js
@@ -0,0 +1,17 @@
+// numbers1.js
+// Doing math in JavaScript is very similar to doing math on paper!
+// We have a function below, can you make it return the sum of two numbers?
+
+// Execute `yarn jslings hint numbers1` for hints!
+
+// DO NOT EDIT ABOVE THIS LINE
+
+// I AM NOT DONE
+
+const sum = (a, b) => {
+    return a;
+};
+
+module.exports = {
+    sum,
+};
---
+++ src/__tests__/numbers/numbers1.test.ts
@@ -0,0 +1,7 @@
+const { sum } = require('../../../exercises/numbers/numbers1.js')
+
+describe("numbers1", () => {
+    it("can add two numbers together", () => {
+        expect(sum(2, 2)).toBe(4);
+    });
+});
