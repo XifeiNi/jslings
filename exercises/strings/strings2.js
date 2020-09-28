602467839a5e0542822368693cba6360fc424123
victorphoenix3 <gaurijove@gmail.com>
2020-09-28 22:36:13 +0530

    Add exercises and tests for strings

---
+++ exercises/strings/strings2.js
@@ -0,0 +1,13 @@
+// strings2.js
+// Use the appropriate string property to make the function return the strings's length
+
+// Execute `yarn jslings hint strings1` for hints!
+
+// DO NOT EDIT ABOVE THIS LINE
+
+const strlen = (text) => {
+    return;
+};
+
+// DO NOT EDIT BELOW THIS LINE
+module.exports =  { strlen };
