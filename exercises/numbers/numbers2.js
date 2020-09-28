44e06de6e4224dbba6b2bcbbcd47c952143c5f90
Anthony Morris <anthony@amorrissound.com>
2020-09-28 09:04:07 -0700

    Adjust generator files

--- exercises/numbers/numbers2.js
+++ exercises/numbers/numbers2.js
@@ -1,7 +1,7 @@
 // numbers2.js
 // Make our `square` function to square a number.
 
-// Execute `yarn jslings hint numbers2` for hints!
+// Press the key 'h' for hints!
 
 // DO NOT EDIT ABOVE THIS LINE
 
