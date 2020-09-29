343b445139a594c5b1ab6cb2d9c6e641adeb1507
Cecilia the Cat <cecilia990@outlook.com>
2020-09-29 15:33:53 +1000

    Add tests for variables (#20)
    * Add tests for variables
    
    * Changed excercises.json
    
    * add new line char

---
+++ exercises/variables/variables1.js
@@ -0,0 +1,21 @@
+// variables1.js
+
+// About this `I AM NOT DONE` thing:
+// We sometimes encourage you to keep trying things on a given exercise,
+// even after you already figured it out. If you got everything working and
+// feel ready for the next exercise, remove the `I AM NOT DONE` comment below.
+
+// I AM NOT DONE
+"use strict";
+
+const constVariable = (a) => {
+    const x = a;
+    x = 2;
+    return x;
+};
+
+// DO NOT EDIT BELOW THIS LINE
+
+module.exports = {
+    constVariable,
+}
